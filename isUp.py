63206187657de37b60cd8cb7817728e1803576d2
Richard T Swierk <18kills.18kills@gmail.com>
2018-04-10 18:44:54 -0400

    Update isUp.py

--- isUp.py
+++ isUp.py
@@ -1,30 +1,34 @@
-import socket
-import os
-import threading
-import sys
+import socket, os, threading, sys
+
+from datetime import datetime
+t1=datetime.now()
 up=[]
+
 s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 localIP=s.getsockname()[0]
 s.close()
 localIP=localIP[:localIP.find(".",localIP.find(".")+localIP.find(".",localIP.find("."))+localIP.find(".",localIP.find(".",localIP.find("."))),)]
+
 def isUp(start,end,step):
     for x in range(start,end,step):
-        check=os.system("ping -w 5 -n 2 "+localIP+"."+str(x+1))
+        check=os.system("ping -w 5 -n 1 "+localIP+"."+str(x+1))
         if check==0:
             up.append(localIP+"."+str(x+1))
+
 def threads():
     try:
         for numThread in range(4):
-            threading.Thread(target=isUp, args=(numThread,20,4)).start()
+            threading.Thread(target=isUp, args=(numThread,255,4)).start()
     except KeyboardInterrupt:
         sys.exit()
-try:
-    activeThreads=threading.activeCount()
-    threads()
-    while threading.activeCount()>activeThreads:
-        pass
-except KeyboardInterrupt:
-    sys.exit()
+
+activeThreads=threading.activeCount()
+threads()
+while threading.activeCount()>activeThreads:
+    pass
+
 for x in range(len(up)):
     print(up[x])
+
+print(datetime.now()-t1)
