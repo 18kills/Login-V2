bf4e68a0dab672bd52eec6acd9e99293ff60daab
Richard T Swierk <18kills.18kills@gmail.com>
2018-04-10 20:36:47 -0400

    Update isUp.py

--- isUp.py
+++ isUp.py
@@ -8,18 +8,17 @@ s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 localIP=s.getsockname()[0]
 s.close()
-localIP=localIP[:localIP.find(".",localIP.find(".")+localIP.find(".",localIP.find("."))+localIP.find(".",localIP.find(".",localIP.find("."))),)]
+localIP=localIP[:localIP.rindex('.')]
 
 def isUp(start,end,step):
     for x in range(start,end,step):
-        check=os.system("ping -w 5 -n 1 "+localIP+"."+str(x+1))
-        if check==0:
+        if os.system("ping -w 5 -n 1 "+localIP+"."+str(x+1))==0:
             up.append(localIP+"."+str(x+1))
 
 def threads():
     try:
-        for numThread in range(4):
-            threading.Thread(target=isUp, args=(numThread,255,4)).start()
+        for numThread in range(15):
+            threading.Thread(target=isUp, args=(numThread,254,15)).start()
     except KeyboardInterrupt:
         sys.exit()
 
