759795dba259fd68348cb28c2e68151f6dfd7ca3
Richard T Swierk <18kills.18kills@gmail.com>
2018-04-16 19:13:23 -0400

    Update isUp.py

--- isUp.py
+++ isUp.py
@@ -1,5 +1,4 @@
 import socket, os, threading, sys
-
 from datetime import datetime
 t1=datetime.now()
 up=[]
@@ -15,18 +14,20 @@ def isUp(start,end,step):
         if os.system("ping -w 5 -n 1 "+localIP+"."+str(x+1))==0:
             up.append(localIP+"."+str(x+1))
 
-def threads():
+def makeThreads(numThreads,function,argument):
     try:
-        for numThread in range(15):
-            threading.Thread(target=isUp, args=(numThread,254,15)).start()
+        for num in range(numThreads):
+            threading.Thread(target=function, args=(num,argument,numThreads)).start()
     except KeyboardInterrupt:
         sys.exit()
-
-activeThreads=threading.activeCount()
-threads()
-while threading.activeCount()>activeThreads:
-    pass
-
+        
+def threads(numThreads,function,argument):
+    activeThreads=threading.activeCount()
+    makeThreads(numThreads,function,argument)
+    while threading.activeCount()>activeThreads:
+        pass
+    
+threads(15,isUp,254)
 for x in range(len(up)):
     print(up[x])
 
